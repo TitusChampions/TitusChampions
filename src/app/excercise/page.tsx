31e10027c5db0f49f7af5a2437e61508a573b45f
Shaina <shainaz@udel.edu>
2026-03-01 10:52:11 -0500

    working on formatting the pillar pages

--- src/app/excercise/page.tsx
+++ src/app/excercise/page.tsx
@@ -1,6 +1,10 @@
 function Excercise(){
     return(
-        <>hello world!</>
+        <div style={{backgroundColor:"white", height:"100vh"}}>
+            <h1 style={{display:"flex",alignItems:"center", justifyContent:"center", color:"#2D77B3"}}>Excercise Page</h1>
+            <hr></hr>
+            <div style={{borderLeft:"1px solid black", height:"100%", marginLeft:"100px"}}></div>
+        </div>
     )
 }
 
