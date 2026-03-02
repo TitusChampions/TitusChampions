c4b86236b74af0688a62d6caebf7d9968229dedc
Shaina <shainaz@udel.edu>
2026-03-02 10:05:29 -0500

    adding figma assets

--- src/app/excercise/page.tsx
+++ src/app/excercise/page.tsx
@@ -6,7 +6,9 @@ function Excercise(){
             <h1 style={{display:"flex",alignItems:"center", justifyContent:"center", color:"#2D77B3"}}>Excercise Page</h1>
             <hr></hr>
             {/* <div style={{borderLeft:"1px solid black", height:"100%", marginLeft:"100px"}}></div> */}
-            <Link href="/rest_recovery" style={{width:"1000px", height:"1000px", backgroundColor:"#2D77B3"}}>Minigames!</Link>
+            <Link href="/excercise_minigames" style={{width:"100%", height:"100px", backgroundColor:"#2D77B3"}}>Minigames!</Link>
+            <Link href="/excercise_minigames" style={{width:"100%", height:"100px", backgroundColor:"#94C951"}}>Tips and Tricks</Link>
+            <Link href="/excercise_minigames" style={{width:"100%", height:"100px", backgroundColor:"#94C951"}}>Quick Workout</Link>
         </div>
     )
 }
